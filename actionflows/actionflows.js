ee203647914ac68ed697f6e2bb8a4d6059148224
Stephen J. Carnam <steve@steveorevo.com>
2017-12-18 14:19:51 -0800

    Support for loops and inc from zero

--- actionflows/actionflows.js
+++ actionflows/actionflows.js
@@ -10,16 +10,25 @@ module.exports = function(RED) {
     }
     var event = "af:" + nodeID;
     var handler = function(msg) {
+      msg._af["noinc0"] = true; // Returning flag, do not increment from zero
       node.receive(msg);
     }
     RED.events.on(event, handler);
     this.on("input", function(msg) {
+
+        // Check loop conditional
+        if (stopLoop()) {
+          node.status({});
+          node.send(msg);
+          return;
+        }
         var af = node.context().global.get('actionflows');
         if (typeof msg._af == 'undefined') {
           msg._af = {};
           msg._af["stack"] = [];
         }
-        if (typeof msg._af[nodeID] == 'undefined') {
+
+        if (typeof msg._af[nodeID] == "undefined") {
           msg._af[nodeID] = {
             execTime: process.hrtime(),
             ins: af[nodeID].ins,
@@ -34,13 +43,160 @@ module.exports = function(RED) {
         }else{
           if (config.perf) {
             var t = process.hrtime(msg._af[nodeID].execTime);
-            node.warn("Execution time: " + t[0] + "s and " + t[1]/1000000 + "ms");
+            node.warn("Action cycle execution time: " + t[0] + "s and " + t[1]/1000000 + "ms");
           }
-          if (msg._af["stack"].length == 0) {
-            delete msg._af;
+          delete msg._af[nodeID];
+
+          // Bump loop and restart action
+          if (config.loop != "none") {
+            if (bumpLoop()) {
+              RED.events.emit(event, msg);
+            }else{
+              // Stop on bump error
+              node.status({fill:"red",shape:"ring",text: "error" });
+              return;
+            }
+          }else{
+            // Move on
+            node.status({});
+            node.send(msg);
           }
-          node.status({});
-          node.send(msg);
+        }
+
+        // Check if stopLoop condition met
+        function stopLoop() {
+
+          // Support increment from zero
+          if (typeof msg._af != "undefined") {
+            if (typeof msg._af["noinc0"] == "undefined" && config.loop == "inc0") {
+              setContextPropertyValue(config.proptype, config.prop, 0);
+            }else{
+              delete msg._af["noinc0"];
+            }
+          }
+
+          var sl = false;
+          if (config.loop == "none") return sl;
+          var prop = getTypeInputValue(config.proptype, config.prop);
+
+          // Initialized undefined variables
+          if (typeof prop == "undefined") {
+
+            // String logic operator inits to empty string
+            if (config.until == "cont" || config.until == "notc") {
+              prop = "";
+            }else{
+
+              // Math logic operator inits to zero
+              prop = 0;
+            }
+            setContextPropertyValue(config.proptype, config.prop, prop);
+          }
+          var untilprop = getTypeInputValue(config.untilproptype, config.untilprop);
+          switch(config.until) {
+            case "eq":
+              sl = (prop == untilprop);
+              break;
+            case "neq":
+              sl = (prop != untilprop);
+              break;
+            case "lt":
+              sl = (prop < untilprop);
+              break;
+            case "lte":
+              sl = (prop <= untilprop);
+              break;
+            case "gt":
+              sl = (prop > untilprop);
+              break;
+            case "gte":
+              sl = (prop >= untilprop);
+              break;
+            case "cont":
+              sl = (prop.indexOf(untilprop) != -1);
+              break;
+            case "notc":
+              sl = (prop.indexOf(untilprop) == -1);
+              break;
+          }
+          return sl;
+        }
+
+        // Bump any loop increment, decrement
+        function bumpLoop() {
+          if (config.loop == "none") return true;
+          var prop = getTypeInputValue(config.proptype, config.prop);
+          switch(config.loop) {
+            case "inc0":
+            case "inc":
+              if (typeof prop == "number") {
+                prop++;
+              }else{
+                node.error("Cannot increment loop variable. " + config.prop + " is not a number.");
+                return false;
+              }
+              break;
+            case "dec":
+              if (typeof prop == "number") {
+                prop--;
+              }else{
+                node.error("Cannot decrement loop variable. " + config.prop + " is not a number.");
+                return false;
+              }
+              break;
+            case "watch":
+              break;
+          }
+          setContextPropertyValue(config.proptype, config.prop, prop);
+          return true;
+        }
+
+        // Decode typeInput value by type/value
+        function getTypeInputValue(t, v) {
+          var r = '';
+          switch(t) {
+            case "msg":
+              r = RED.util.getMessageProperty(msg, v);
+              break;
+            case "flow":
+              r = flowContext.get(v);
+              break;
+            case "global":
+              r = globalContext.get(v);
+              break;
+            case "str":
+              try {
+                r = unescape(JSON.parse('"'+v+'"'));;
+              }catch(e){
+                r = v;
+              }
+              break;
+            case "num":
+              r = parseFloat(v);
+              break;
+            case 'bool':
+              r = (v=='true');
+              break;
+            default:
+              r = v;
+          }
+          return r;
+        }
+
+        // Set the context property value
+        function setContextPropertyValue(context, property, value) {
+            // Assign value to given object and property
+            switch(context) {
+              case "msg":
+                RED.util.setMessageProperty(msg, property, value);
+                break;
+              case "flow":
+                flowContext.set(property, value);
+                break;
+              case "global":
+                globalContext.set(property, value);
+                break;
+            }
         }
     });
     this.on("close",function() {
@@ -108,6 +264,7 @@ module.exports = function(RED) {
       ins.forEach(function(i) {
         if (S(i.name).replaceAll("_", " ") // search for prefix while preventing
                      .replaceAll("-", " ") // substr match (i.e. 'he' in 'head')
+                     .replaceAll(".", " ") // support domain format
                      .startsWith(a.name + " ")) {
           a.ins.push(i);
         }
