dedda2d93020c5b6c1bac3f19f8cefee214780b2
Stephen J. Carnam <steve@steveorevo.com>
2018-01-02 19:54:52 -0800

    purge old objects on deployment

--- actionflows/actionflows.js
+++ actionflows/actionflows.js
@@ -1,17 +1,130 @@
 module.exports = function(RED) {
+  var S = require('string');
   RED.nodes.registerType("actionflows", actionflows);
   function actionflows(config) {
     RED.nodes.createNode(this, config);
-    var node = this;
-    node.context().global.set('actionflows', getActionFlows());
     var nodeID = config.id;
-    if (typeof config._alias != 'undefined') {
-      nodeID = config._alias;
+    var node = this;
+
+    // Gather all `action in`
+    var RED2 = require.main.require('node-red');
+    var flows = RED2.nodes.getFlows().flows;
+
+    var ai = [];
+    flows.forEach(function(f) {
+      if (f.type == 'actionflows_in') {
+        ai.push(f);
+      }
+    });
+
+    // Find all `action in` related by this actionflows' name
+    var ins = [];
+
+    // Identify instance
+    var instance = config.name;
+    var subflow = getByID(config.z);
+    if (subflow != null) {
+      if (typeof subflow.name != "undefined") {
+        instance = subflow.name + " " + config.name;
+      }
     }
+
+    ai.forEach(function(f) {
+      // Look for any `action in` on this level
+      if (typeof config._alias == "undefined") {
+
+        // Any tab, as long as we're not in a subflow
+        if (prefixMatch(f.name).startsWith(prefixMatch(config.name))) {
+          ins.push(f);
+        }
+      }else{
+
+        // Restrict to this tab
+        if (f.z == config.z && prefixMatch(f.name).startsWith(prefixMatch(config.name))) {
+          ins.push(f);
+        }
+      }
+    });
+    ai.forEach(function(f) {
+      // Look for any `action in` within the subflow instance
+      var alias = getByID(config._alias);
+      if (alias != null) {
+        if (f.z == alias.z && prefixMatch(f.name).startsWith(prefixMatch(alias.name))) {
+          ins.push(f);
+        }
+      }
+    });
+    ai.forEach(function(f) {
+      // Look for any `action in` on first namespace'd (subflow) level
+      if (subflow != null) {
+        if (f.z == subflow.z && prefixMatch(f.name).startsWith(prefixMatch(subflow.name + " " + config.name))) {
+          ins.push(f);
+        }
+      }
+    });
+
+    // Sort all `action in` by priority
+    ins.sort(function(a, b) {
+      return parseInt(a.priority)-parseInt(b.priority);
+    });
+
+    // Enforce private settings
+    var zz = config.z;
+    if (typeof config._alias != "undefined") {
+      zz = getByID(config._alias).z;
+    }
+    var pins = [];
+    ins.forEach(function(i) {
+      if ((i.private == false && config.private == false) ||
+          (config.private == true && i.z == zz) ||
+          (i.private == true && i.z == zz)) {
+            pins.push(i);
+      }
+    });
+    ins = pins;
+
+    // Associate all `action in` with their actionflows
+    var af = node.context().global.get('actionflows');
+    if (typeof af == "undefined") {
+      af = new Object();
+    }
+    if (typeof af[config.id] == "undefined") {
+      af[config.id] = config;
+    }
+    var seconds = new Date().getTime() / 1000;
+    af[config.id].age = seconds;
+    af[config.id].ins = ins;
+
+    // Purge old actionflows not deployed within the last 10 seconds
+    for(var id in af) {
+      if ((seconds - af[id].age) > 10) {
+        delete af[id];
+      }
+    }
+    node.context().global.set('actionflows', af);
+
+    // Util functions
+    function prefixMatch(s) {
+      s = S(s).replaceAll("_", " ").replaceAll("-", " ").replaceAll(".", " ");
+      return s.toString();
+    }
+    function getByID(id) {
+      for (var i = 0; i < flows.length; i++) {
+        var f = flows[i];
+        if (f.id == id) {
+          return f;
+          break;
+        }
+      }
+      return null;
+    }
+
     var event = "af:" + config.id;
     var handler = function(msg) {
-      msg._af["noinc0"] = true; // Returning flag, do not increment from zero
-      node.receive(msg);
+      if (typeof msg._af != "undefined") {
+        msg._af["noinc0"] = true; // Returning flag, do not increment from zero
+        node.receive(msg);
+      }
     }
     RED.events.on(event, handler);
     this.on("input", function(msg) {
